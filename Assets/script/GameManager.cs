d7ab277d57a48803e85ee33fc08224156b6fa40e
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-09-18 11:20:17 -0300

    giitgnore

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -7,14 +7,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     #region singleton
-        public static GameManager instance;
+    public static GameManager instance;
     #endregion
 
     public GameObject jogador1;
     public GameObject jogador2;
     public GameObject bloco;
 
-   
+
 
     int linhas = 10;
     int colunas = 10;
@@ -27,15 +27,15 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
         matriz = new int[linhas, colunas];
-        CriarGrade();  
+        CriarGrade();
     }
     void CriarGrade()
     {
         for (int i = 0; i < linhas; i++)
         {
             for (int j = 0; j < colunas; j++)
-            {  
-                Instantiate(bloco, new Vector3(i*espacoLinhas, j*espacoLinhas), Quaternion.identity);//cria 100 blocos (só falta mudar a posição para poder ficar baita)
+            {
+                Instantiate(bloco, new Vector3(i * espacoLinhas, j * espacoLinhas), Quaternion.identity);//cria 100 blocos (só falta mudar a posição para poder ficar baita)
             }
         }
     }
