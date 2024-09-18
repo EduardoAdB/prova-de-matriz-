74be2db9059880678199e0577179419123294be3
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-09-18 13:56:50 -0300

    game manager finalizado
    eu não aguento mais
    socorro

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,6 +16,9 @@ public class GameManager : MonoBehaviour
     public GameObject bloco;
 
     public float distanciaCamera = 10.0f;
+
+    int blocosVerdes = 0;
+    int blocosVermelhos = 0;
 
     int linhas = 10;
     int colunas = 10;
@@ -40,14 +44,57 @@ public class GameManager : MonoBehaviour
         }
         Vector3 centroMatriz = new Vector3((linhas - 1) * espacoLinhas / 2, (colunas - 1) * espacoLinhas / 2, -distanciaCamera);
         Camera.main.transform.position = centroMatriz;
+        Camera.main.orthographicSize = Mathf.Max(linhas, colunas) * espacoLinhas / 2;
 
-        Vector3 posicaoInicialJogador1 = new Vector3(0, 0, -1 * espacoLinhas); 
-        Vector3 posicaoInicialJogador2 = new Vector3((linhas - 1) * espacoLinhas, 0, (colunas - 1) * espacoLinhas);
+        Vector3 posicaoJogador1 = new Vector3(0, 0, 0);//mudar posição do spawn
+        Vector3 posicaoJogador2 = new Vector3((linhas - 1) * espacoLinhas, 0, (colunas - 1) * espacoLinhas);  //mudar spawn
 
-        Instantiate(jogador1, posicaoInicialJogador1, Quaternion.identity);
-        Instantiate(jogador2, posicaoInicialJogador2, Quaternion.identity);
-        
+        Instantiate(jogador1, posicaoJogador1, Quaternion.identity);
+        Instantiate(jogador2, posicaoJogador2, Quaternion.identity);
+    }
+    int pertenceAoJd1;
+    int pertenceAoJd2;
 
+    public void ConquistarTerritorio()
+    {
+
+        territoriosConquistados++;
+        if (territoriosConquistados == matriz.Length)
+        {  
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (matriz[i, j] == pertenceAoJd1)
+                    {
+                        blocosVerdes++;
+                    }
+                    else if (matriz[i,j] == pertenceAoJd2)
+                    {
+                        blocosVermelhos++;
+                    }
+                }
+            }
+            FimDeJogo(blocosVerdes,blocosVermelhos);
+        }
+    }
+    public void FimDeJogo(int blocosVerdes, int blocosVermelhos)
+    {
+        string vencedor;
+        if (blocosVerdes > blocosVermelhos)
+        {
+            vencedor = "Jogador 1 é o vencedor! ";// + " dominou " + blocosVerdes + " territorios";
+        }
+        else if (blocosVerdes < blocosVermelhos)
+        {
+            vencedor = "Jogador 2 é o vencedor! ";
+        }
+        else
+        {
+            vencedor = "O jogo terminou em impate";
+        }
+
+        Debug.Log(vencedor);
     }
     
 
