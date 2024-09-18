5d88381caef6b77b32e22ef03a576504080c0ab4
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-09-18 11:40:33 -0300

    bolas(jogador)

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
     public GameObject jogador2;
     public GameObject bloco;
 
-
+    public float distanciaCamera = 10.0f;
 
     int linhas = 10;
     int colunas = 10;
@@ -38,8 +38,16 @@ public class GameManager : MonoBehaviour
                 Instantiate(bloco, new Vector3(i * espacoLinhas, j * espacoLinhas), Quaternion.identity);//cria 100 blocos (só falta mudar a posição para poder ficar baita)
             }
         }
-        Instantiate(jogador1, new Vector3(1, 1 ,1), Quaternion.identity);
-        Instantiate(jogador2, new Vector3(1, 1, 1), Quaternion.identity);
+        Vector3 centroMatriz = new Vector3((linhas - 1) * espacoLinhas / 2, (colunas - 1) * espacoLinhas / 2, -distanciaCamera);
+        Camera.main.transform.position = centroMatriz;
+
+        Vector3 posicaoInicialJogador1 = new Vector3(0, 0, -1 * espacoLinhas); 
+        Vector3 posicaoInicialJogador2 = new Vector3((linhas - 1) * espacoLinhas, 0, (colunas - 1) * espacoLinhas);
+
+        Instantiate(jogador1, posicaoInicialJogador1, Quaternion.identity);
+        Instantiate(jogador2, posicaoInicialJogador2, Quaternion.identity);
+        
+
     }
     
 
